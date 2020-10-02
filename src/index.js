937809254d88b95c1ba5d70256de0339bb283c51
Rozum Alena <rozum.elena@gmail.com>
2020-10-02 23:41:46 +0300

    feat: decided

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+} else {
+  array.sort( (a, b) => a - b);
+  return array[0];
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+} else {
+  array.sort( (a, b) => a - b).reverse();
+  return array[0];
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+      return 0;
+  } else {
+  const lengthArr = array.length;
+  const sumAll = array.reduce((acc,item) => acc + item);
+  return sumAll / lengthArr;
+  } 
 }
+
